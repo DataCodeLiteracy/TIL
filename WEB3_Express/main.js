1355748b6b61a4c82263fe7dfab0434f6004ae7c
DataCodeLiteracy <dataliteracy@icloud.com>
2023-04-02 17:54:30 +0900

    Express Hello World 2

--- WEB3_Express/main.js
+++ WEB3_Express/main.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+  res.send('/');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get('/page', (req, res) => {
+  res.send('/page');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000');
 });
