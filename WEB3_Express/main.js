341e7f4115b70470082a366fc80c6f95b72ef956
DataCodeLiteracy <dataliteracy@icloud.com>
2023-04-02 19:26:52 +0900

    Express HomePage

--- WEB3_Express/main.js
+++ WEB3_Express/main.js
@@ -1,15 +1,29 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
+const fs = require('fs');
+const template = require('./lib/template.js');
 
 app.get('/', (req, res) => {
-  res.send('/');
+  fs.readdir('./data', (error, filelist) => {
+    const title = 'Welcome';
+    const description = 'Hello, Node.js';
+    const list = template.list(filelist);
+    const html = template.HTML(
+      title,
+      list,
+      `<h2>${title}</h2>${description}`,
+      '<a href="/create">create</a>',
+    );
+    res.send(html);
+  });
 });
 
 app.get('/page', (req, res) => {
   res.send('/page');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
