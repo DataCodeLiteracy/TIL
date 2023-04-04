588574c3c63b3abbc32ef5088c5db272afa28af2
DataCodeLiteracy <dataliteracy@icloud.com>
2023-04-04 15:24:46 +0900

    create page

--- WEB3_Express/main.js
+++ WEB3_Express/main.js
@@ -5,6 +5,7 @@ const port = 3000;
 const fs = require('fs');
 const path = require('path');
 const sanitizeHtml = require('sanitize-html');
+const qs = require('querystring');
 const template = require('./lib/template.js');
 
 app.get('/', (req, res) => {
@@ -44,6 +45,41 @@ app.get('/page/:pageId', (req, res) => {
           </form>`,
       );
       res.send(html);
+    });
+  });
+});
+
+app.get('/create', (req, res) => {
+  fs.readdir('./data', (error, filelist) => {
+    const title = 'WEB - create';
+    const list = template.list(filelist);
+    const html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    res.send(html);
+  });
+});
+
+app.post('/create_process', (req, res) => {
+  let body = '';
+  req.on('data', (data) => {
+    body += data;
+  });
+  req.on('end', () => {
+    const post = qs.parse(body);
+    const { title } = post;
+    const { description } = post;
+    fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+      res.writeHead(302, { Location: `/?id=${title}` });
+      res.end();
     });
   });
 });
