c19ebe44ab61e4a8e7b7ce5189c6c40f169afa79
DataCodeLiteracy <dataliteracy@icloud.com>
2023-04-07 03:31:37 +0900

    discriminated Union

---
+++ TypeScript/DreamCoding/1-types/1-6-discriminated.ts
@@ -0,0 +1,46 @@
+{
+  // function: login -> success, fail
+  type SuccessState = {
+    result: 'success';
+    response: {
+      body: string;
+    };
+  }
+
+  let success: SuccessState = {
+    result: 'success',
+    response: { body: '성공이다!!' }
+  };
+  let fail: FailState = {
+    result: 'fail',
+    reason: '실패다!!'
+  }
+
+  type FailState = {
+    result: 'fail';
+    reason: string;
+  }
+  type LoginState = SuccessState | FailState
+  function login(): LoginState {
+    return {
+      result: 'success',
+      response: {
+        body: 'logged in!',
+      },
+    };
+  }
+
+  // printLoginState(state)
+  // success -> body
+  // fail -> reason
+  function printLoginState(state: LoginState) {
+    if (state.result === 'success') {
+      console.log(state.response.body);
+    } else {
+      console.log(state.reason);
+    }
+  }
+
+  printLoginState(success);
+  printLoginState(fail);
+}
