5b8ab86afe33e19c32a7f9ba532d8e073627f540
DataCodeLiteracy <dataliteracy@icloud.com>
2023-04-16 08:37:32 +0900

    lecture: list & function

--- WEB2_Node.js/main.js
+++ WEB2_Node.js/main.js
@@ -2,6 +2,32 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+const templateHTML = (title, list, body) => `
+    <!doctype html>
+    <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+          ${list}
+          ${body}
+      </body>
+    </html>
+  `;
+
+const checkList = (fileList) => {
+  let list = "<ol>";
+  let i = 0;
+  while (i < fileList.length) {
+    list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+    i++;
+  }
+  return list;
+  list += "</ol>";
+};
+
 const app = http.createServer((request, response) => {
   const _url = request.url;
   const queryData = url.parse(_url, true).query;
@@ -9,53 +35,31 @@ const app = http.createServer((request, response) => {
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      const title = "Welcome";
-      const description = "Hello! Node.js!";
-      const template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      const title = queryData.id;
-      fs.readFile(`data/${title}`, "utf8", (err, description) => {
-        const template = `
-          <!doctype html>
-          <html>
-          <head>
-          <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${description}</p>
-          </body>
-          </html>
-        `;
+      fs.readdir("./data", (err, fileList) => {
+        const title = "Welcome";
+        const description = "Hello! Node.js!";
+        const list = checkList(fileList);
+        const template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2><p>${description}</p>`
+        );
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      const title = queryData.id;
+      fs.readdir("./data", (err, fileList) => {
+        fs.readFile(`data/${title}`, "utf8", (err, description) => {
+          const list = checkList(fileList);
+          const template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2><p>${description}</p>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
